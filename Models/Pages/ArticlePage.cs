64580556b6c95d05a3494a61cfa563c7a68a49a9
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-09-19 19:34:03 +0200

    Added logging

---
+++ Models/Pages/ArticlePage.cs
@@ -0,0 +1,19 @@
+﻿using Optimizely002.Business;
+using System.ComponentModel.DataAnnotations;
+
+namespace Optimizely002.Models.Pages
+{
+    [ContentType(
+    GUID = "AAE76A1F-C82F-4EB7-87A7-ED24083369B1",
+    GroupName = Globals.GroupNames.Specialized
+    )]
+    public class ArticlePage : SitePageData
+    {
+        [Display(
+            GroupName = SystemTabNames.Content,
+            Order = 10
+            )]
+        [CultureSpecific]
+        public virtual string Heading { get; set; } = string.Empty;
+    }
+}
