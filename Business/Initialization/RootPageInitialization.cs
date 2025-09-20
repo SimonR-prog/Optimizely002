0d45a3886211b8727531bf3258c65b696ba2cae6
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-09-20 18:13:42 +0200

    Added initialize module

---
+++ Business/Initialization/RootPageInitialization.cs
@@ -0,0 +1,29 @@
+﻿using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.Initialization;
+using EPiServer.ServiceLocation;
+using Optimizely002.Models.Pages;
+
+namespace Optimizely002.Business.Initialization;
+
+
+[InitializableModule]
+[ModuleDependency(typeof(CmsCoreInitialization))]
+public class RootPageInitialization
+{
+    public void Initialize(InitializationEngine context)
+    {
+        var contentTypeRepository = context.Locate.Advanced.GetInstance<IContentTypeRepository>();
+        var sysRoot = contentTypeRepository.Load("SysRoot") as PageType;
+        var setting = new AvailableSetting { Availability = Availability.Specific };
+
+        setting.AllowedContentTypeNames.Add(contentTypeRepository.Load<StartPage>().Name);
+
+        var avaliableSettingsRepository = context.Locate.Advanced.GetInstance<IAvailableSettingsRepository>();
+        avaliableSettingsRepository.RegisterSetting(sysRoot, setting);
+    }
+
+    public void Uninitialize(InitializationEngine context)
+    {
+    }
+}
