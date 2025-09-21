a27d7d6a94857adaa7cd942c9f66f634ebf539dd
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-09-21 14:28:22 +0200

    Meta data extender and initialization.

---
+++ Business/Initialization/MetaDataInitialization.cs
@@ -0,0 +1,28 @@
+﻿using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.Initialization;
+using EPiServer.ServiceLocation;
+using EPiServer.Shell.ObjectEditing;
+using Optimizely002.Business.Extenders;
+using Optimizely002.Models.Pages;
+
+namespace Optimizely002.Business.Initialization;
+
+
+[InitializableModule]
+[ModuleDependency(typeof(CmsCoreInitialization))]
+public class MetaDataInitialization : IInitializableModule
+{
+    public void Initialize(InitializationEngine context)
+    {
+        if (context.HostType == HostType.WebApplication) 
+        { 
+            var registry = context.Locate.Advanced.GetInstance<MetadataHandlerRegistry>();
+            registry.RegisterMetadataHandler(typeof(ContentData), new MetaDataExtender());
+        }
+    }
+
+    public void Uninitialize(InitializationEngine context)
+    {
+    }
+}
