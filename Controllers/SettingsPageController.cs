03a5bed060821e3350c0eaae374906f0e94ed369
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-09-22 22:58:19 +0200

    Settings page controller, index, viewmodel

---
+++ Controllers/SettingsPageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Optimizely002.Models.Pages;
+using Optimizely002.Models.ViewModels;
+
+namespace Optimizely002.Controllers;
+
+public class SettingsPageController : PageControllerBase<SettingsPage> 
+{
+    public IActionResult Index(SettingsPage currentPage)
+    {
+        var model = new SettingsPageViewModel(currentPage);
+
+        return View(model);
+    }
+}
---
+++ Models/ViewModels/SettingsPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Optimizely002.Models.Pages;
+
+namespace Optimizely002.Models.ViewModels;
+
+public class SettingsPageViewModel : PageViewModel<SettingsPage>
+{
+    public SettingsPageViewModel(SettingsPage currentPage) : base(currentPage)
+    {
+    }
+}
