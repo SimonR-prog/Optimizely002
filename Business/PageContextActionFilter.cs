fc038e9892e53d61054438cd619b3557f4e0aa23
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-09-22 20:38:00 +0200

    Page context action filter

---
+++ Business/PageContextActionFilter.cs
@@ -0,0 +1,41 @@
+﻿using EPiServer.Web.Routing;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Optimizely002.Models.Pages;
+using Optimizely002.Models.ViewModels;
+
+namespace Optimizely002.Business;
+
+public class PageContextActionFilter : IActionFilter
+{
+    private readonly PageViewContextFactory _contextFactory;
+
+    public PageContextActionFilter(PageViewContextFactory contextFactory)
+    {
+        _contextFactory = contextFactory;
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        var controller = context.Controller as Controller;
+        var viewModel = controller?.ViewData.Model;
+
+        if (viewModel is IPageViewModel<SitePageData> model)
+        {
+            var currentContentLink = context.HttpContext.GetContentLink();
+            var layoutModel = model.Layout ?? _contextFactory.CreateLayoutModel(currentContentLink, context.HttpContext);
+
+            if (context.Controller is IModifyLayout layoutController)
+            {
+                layoutController.ModifyLayout(layoutModel);
+            }
+
+            model.Layout = layoutModel;
+        }
+    }
+
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+    }
+
+}
